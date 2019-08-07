2771b7dac2462c237cd5d741ce93e0b10eb320bb
王振 <wangzhen@fit2cloud.com>
2019-08-07 09:43:05 +0800

    update

---
+++ main.tf
@@ -0,0 +1,24 @@
+variable "ak" {
+  type = string
+}
+variable "sk" {
+  type = string
+}
+provider "aws" {
+  region  = "cn-northwest-1"
+  access_key = "${var.ak}"
+  secret_key = "${var.sk}"
+}
+resource "aws_s3_bucket" "cmp" {
+  bucket        = "jenkins-terraform-test"
+  acl           = "private"
+  force_destroy = true
+
+  tags = {
+    Name      = "jenkins-terraform-test"
+    Terraform = true
+  }
+}
+output "s3_bucket" {
+  value = "${aws_s3_bucket.cmp}"
+}
